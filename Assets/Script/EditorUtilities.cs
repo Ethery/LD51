c8135be4a6bf9902d92329ea19249909dbf99b29
Ethery <loic.meunier74@gmail.com>
2022-10-01 13:08:28 +0200

    added Ctrl+G to Start game from different scene.

---
+++ Assets/Script/EditorUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EditorUtilities : ScriptableObject
+{
+	static List<Scene> CurrentScenes = new List<Scene>();
+
+	[MenuItem("Tools/Launch game %g")]
+	static void Open()
+	{
+		SceneAsset myWantedStartScene = AssetDatabase.LoadAssetAtPath<SceneAsset>("Assets/Scenes/MainMenu.unity");
+		if (myWantedStartScene != null)
+			EditorSceneManager.playModeStartScene = myWantedStartScene;
+		else
+			Debug.Log("Could not find Scene Assets/Scenes/MainMenu.unity");
+		EditorApplication.isPlaying = true;
+	}
+}
